4eed2dabe4c31c4532d89f8073761c15cb6ba4a6
qlulezz <54777265+qlulezz@users.noreply.github.com>
2026-06-07 00:52:26 +0200

    feat: oculus scoresaber avatars

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -10,11 +10,13 @@ export default function Avatar({ id }: AvatarProps) {
   // Multiplayer+ stores Oculus and Steam IDs similar to ScoreSaber
   // Use Oculus icon by default, switch to Steam avatar if detected
   const isSteamId = id.startsWith("76561") && id.length === 17;
-  const url = isSteamId ? `${SS_CDN}/${id}.jpg` : `${SS_CDN}/oculus.png`;
+  const url = `${SS_CDN}/${id}.jpg`;
 
   // Replace the image url with default Steam '?' avatar if something breaks
   const handleError = (e: React.SyntheticEvent<HTMLImageElement>) => {
-    e.currentTarget.src = `${SS_CDN}/steam.png`;
+    e.currentTarget.src = isSteamId
+      ? `${SS_CDN}/steam.png`
+      : `${SS_CDN}/oculus.png`;
   };
 
   return (
